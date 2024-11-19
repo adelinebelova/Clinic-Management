91c583239a40fdbba6d6d142a7123029c29b9d45
adelinebelova <63752238+adelinebelova@users.noreply.github.com>
2024-11-18 22:41:55 -0500

    Established WebAPI boiler code

--- Library.Clinic/obj/Debug/net8.0/Library.Clinic.AssemblyInfo.cs
+++ Library.Clinic/obj/Debug/net8.0/Library.Clinic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.Clinic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cff09e45d570ce33d18f87ceeda1e1c25572982")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61073ac95aa73937cffc3d5ae00967f2801ddf59")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.Clinic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.Clinic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
